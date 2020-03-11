d87cf85fca766c5c47d3994f2064b8bb61c4a032
Emily Clinkscales <emclinks@github.com>
2020-03-10 23:28:30 -0400

    Main completion of Project 07

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
 
-
 func _physics_process(_delta: float) -> void:
 	_velocity.x *= -1 if is_on_wall() else 1
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
